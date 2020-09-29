eee00f1723f078946fa2e045b0f57c9c4896e91e
Gokhul Sakthivel <34570567+gokhulsakthivel@users.noreply.github.com>
2020-09-29 21:32:39 +0530

    Updates

---
+++ precache-manifest.aeca4387bdf90afe3361c8c26e3908a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84a1b03d0904cd97b0ac36645077f375",
+    "url": "/Todos/index.html"
+  },
+  {
+    "revision": "f50fc92e65ce73b4165f",
+    "url": "/Todos/static/css/main.7210e68c.chunk.css"
+  },
+  {
+    "revision": "4eebaac7e083b27f068d",
+    "url": "/Todos/static/js/2.73fe92ee.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Todos/static/js/2.73fe92ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f50fc92e65ce73b4165f",
+    "url": "/Todos/static/js/main.0c26a950.chunk.js"
+  },
+  {
+    "revision": "1471ff196e53b9b5dee9",
+    "url": "/Todos/static/js/runtime-main.7773c5ff.js"
+  },
+  {
+    "revision": "88be4435c0e914d857145f893fc887f6",
+    "url": "/Todos/static/media/view-of-elephant.88be4435.jpg"
+  }
+]);
